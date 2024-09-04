fb26c3a9522ebc980be463ad73b0cf9c60b5fd5f
sknek <sknek@ya.ru>
2024-09-05 01:24:53 +0300

    pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import React from 'react'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 
 import "./App.scss"
 
 import Layout from './Layout'
 import NotFoundPage from './pages/NotFoundPage'
+import AllCampsPage from './pages/AllCampsPage'
+import YourCampsPage from './pages/YourCampsPage'
+import YourPostsPage from './pages/YourPostsPage'
 
 export default function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Layout>
         <Routes>
+          <Route path="/" element={<Navigate to="/campaigns/" />} />
+          <Route path="/campaigns" element={<Navigate to="/campaigns/" />} />
+          <Route path="/campaigns/" element={<AllCampsPage />} />
+          <Route path="/campaigns/:addr" element={<YourCampsPage />} />
+          <Route path="/posts/:addr" element={<YourPostsPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Layout>
--- src/Layout.js
+++ src/Layout.js
@@ -1,6 +1,15 @@
+import { Link } from "react-router-dom";
+
 export default function ({ children }) {
   return (
     <div>
+      <div>
+        <ul>
+          <li><Link to="/campaigns/">All Campaigns</Link></li>
+          <li><Link to="/campaigns/addr">Your Campaigns</Link></li>
+          <li><Link to="/posts/addr">Your Posts</Link></li>
+        </ul>
+      </div>
       {children}
     </div>
   )
