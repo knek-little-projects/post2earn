a31ece202394fee9b100db4f63806069e015879d
sknek <sknek@ya.ru>
2024-09-05 01:52:30 +0300

    sample camps

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-import { TokenModel, UserModel } from "./data"
+import CAMPAIGNS from "../data/sample/campaigns.json"
 
 const API_BASE_URL = process.env.REACT_APP_BACK_URL || "http://localhost:8000"
 
@@ -13,16 +13,6 @@ async function get(path) {
   const result = await response.json()
   console.debug("GET", path, result)
   return result
-}
-
-export function getUserId() {
-  const tgUser = window.Telegram?.WebApp?.initDataUnsafe?.user
-
-  if (tgUser || process.env.NODE_ENV === "production") {
-    return parseInt(tgUser.id)
-  } else {
-    return parseInt(window.localStorage.getItem("id"))
-  }
 }
 
 async function post(path, data) {
@@ -40,35 +30,6 @@ async function post(path, data) {
   return result
 }
 
-export async function login() {
-  const tgUser = window.Telegram?.WebApp?.initDataUnsafe?.user
-  const data = {}
-
-  const friendId = parseInt(window.localStorage.getItem("friendId"))
-  data.friendId = friendId
-
-  if (tgUser || process.env.NODE_ENV === "production") {
-    data.id = tgUser.id
-    data.username = tgUser.username
-    data.first_name = tgUser.first_name
-    data.last_name = tgUser.last_name
-    data.language_code = tgUser.language_code
-    data.allows_write_to_pm = tgUser.allows_write_to_pm
-    data.is_premium = tgUser.is_premium
-  } else {
-    data.id = getUserId()
-    data.username = window.localStorage.getItem("username")
-    data.first_name = window.localStorage.getItem("first_name")
-    data.last_name = window.localStorage.getItem("last_name")
-    data.language_code = window.localStorage.getItem("language_code")
-    data.allows_write_to_pm = window.localStorage.getItem("allows_write_to_pm")
-    data.is_premium = window.localStorage.getItem("is_premium")
-  }
-
-  if (!data.id) {
-    throw Error(`Error loading Telegram.WebApp`)
-  }
-
-  window.localStorage.setItem('X-USER-ID', data.id)
-  return new UserModel(await post("/login", data))
+async function getCampaigns() {
+  return CAMPAIGNS
 }
