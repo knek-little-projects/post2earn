ea3d49cfbcb352e379a0cbfde75c3bf92643f39b
sknek <sknek@ya.ru>
2024-09-07 00:20:18 +0300

    some elements

--- src/components/CampView.js
+++ src/components/CampView.js
@@ -2,49 +2,54 @@ import "./CampView.scss"
 
 export default function ({ item }) {
   return (
-    <div className="camp-view">
-      <div className="camp-field">
-        <strong>ID:</strong> <span>{item.id}</span>
+    <div>
+
+      <div className="camp-view">
+        <div className="camp-field">
+          <strong>ID:</strong> <span>{item.id}</span>
+        </div>
+        <div className="camp-field">
+          <strong>Title:</strong> <span>{item.title}</span>
+        </div>
+        <div className="camp-field">
+          <strong>Owner:</strong> <span>{item.owner}</span>
+        </div>
+        <div className="camp-field">
+          <strong>Post Requirement Text:</strong> <span>{item.postRequirements.text}</span>
+        </div>
+        <div className="camp-field">
+          <strong>Post Requirement Link:</strong> <span>{item.postRequirements.link}</span>
+        </div>
+        <div className="camp-field">
+          <strong>Post Requirement Tag:</strong> <span>{item.postRequirements.tag}</span>
+        </div>
+        <div className="camp-field">
+          <strong>Min Followers:</strong> <span>{item.bloggerRequirements.minFollowers}</span>
+        </div>
+        <div className="camp-field">
+          <strong>Favorite Topic:</strong> <span>{item.bloggerRequirements.favoriteTopic}</span>
+        </div>
+        <div className="camp-field">
+          <strong>Post Min Time To Live:</strong> <span>{item.bloggerRequirements.postMinTimeToLive}</span>
+        </div>
+        <div className="camp-field">
+          <strong>Payment Token:</strong> <span>{item.paymentDetails.paymentToken}</span>
+        </div>
+        <div className="camp-field">
+          <strong>Payment Per Post:</strong> <span>{item.paymentDetails.paymentPerPost} {item.paymentDetails.symbol}</span>
+        </div>
+        <div className="camp-field">
+          <strong>Total Supply:</strong> <span>{item.paymentDetails.totalSupply} {item.paymentDetails.symbol}</span>
+        </div>
+        <div className="camp-field">
+          <strong>Locked Supply:</strong> <span>{item.paymentDetails.lockedSupply} {item.paymentDetails.symbol}</span>
+        </div>
+        <div className="camp-field">
+          <strong>Claimed Supply:</strong> <span>{item.paymentDetails.claimedSupply} {item.paymentDetails.symbol}</span>
+        </div>
       </div>
-      <div className="camp-field">
-        <strong>Title:</strong> <span>{item.title}</span>
-      </div>
-      <div className="camp-field">
-        <strong>Owner:</strong> <span>{item.owner}</span>
-      </div>
-      <div className="camp-field">
-        <strong>Post Requirement Text:</strong> <span>{item.postRequirements.text}</span>
-      </div>
-      <div className="camp-field">
-        <strong>Post Requirement Link:</strong> <span>{item.postRequirements.link}</span>
-      </div>
-      <div className="camp-field">
-        <strong>Post Requirement Tag:</strong> <span>{item.postRequirements.tag}</span>
-      </div>
-      <div className="camp-field">
-        <strong>Min Followers:</strong> <span>{item.bloggerRequirements.minFollowers}</span>
-      </div>
-      <div className="camp-field">
-        <strong>Favorite Topic:</strong> <span>{item.bloggerRequirements.favoriteTopic}</span>
-      </div>
-      <div className="camp-field">
-        <strong>Post Min Time To Live:</strong> <span>{item.bloggerRequirements.postMinTimeToLive}</span>
-      </div>
-      <div className="camp-field">
-        <strong>Payment Token:</strong> <span>{item.paymentDetails.paymentToken}</span>
-      </div>
-      <div className="camp-field">
-        <strong>Payment Per Post:</strong> <span>{item.paymentDetails.paymentPerPost} {item.paymentDetails.symbol}</span>
-      </div>
-      <div className="camp-field">
-        <strong>Total Supply:</strong> <span>{item.paymentDetails.totalSupply} {item.paymentDetails.symbol}</span>
-      </div>
-      <div className="camp-field">
-        <strong>Locked Supply:</strong> <span>{item.paymentDetails.lockedSupply} {item.paymentDetails.symbol}</span>
-      </div>
-      <div className="camp-field">
-        <strong>Claimed Supply:</strong> <span>{item.paymentDetails.claimedSupply} {item.paymentDetails.symbol}</span>
-      </div>
+
     </div>
-  );
-};
+
+  )
+}
--- src/pages/CampPage.js
+++ src/pages/CampPage.js
@@ -4,6 +4,14 @@ import * as api from "../api"
 import useAsyncRequest from "../hooks/useAsyncRequest"
 import MultiLoader from "../components/MultiLoader"
 
+const STATUS_EMPTY = 0
+const STATUS_BOOKED = 1
+const STATUS_SUBMITTED = 2
+const STATUS_FAILED = 3
+const STATUS_FIRST_SUCCEED = 4
+const STATUS_FINAL_SUCCEED = 5
+
+
 export default function () {
   const { id } = useParams()
   const profileLoader = useAsyncRequest(api.getProfile)
@@ -12,11 +20,95 @@ export default function () {
   const camp = campLoader.data
   const profile = profileLoader.data
 
-  const isOwner = profile?.address === camp?.owner
-
   return (
     <MultiLoader loaders={[profileLoader, campLoader]}>
-      <CampView item={camp} isOwner={isOwner} />
+      {
+        camp
+        &&
+        profile
+        &&
+        <>
+          <h1>
+            {camp.title}
+          </h1>
+          <div>
+            <h2>Campaign controls</h2>
+            <button>STOP CAMPAIGN</button>
+            <button>Withdraw available funds</button>
+          </div>
+          <div>
+            <div>
+              <h2>Want to earn?</h2>
+              <button>POST TO EARN</button>
+            </div>
+            <div>
+              <h2>Post status</h2>
+              <div>You've booked a slot and have 10 minutes to make the post</div>
+              <div>
+                What to post
+              </div>
+              <button>I'VE POSTED!</button>
+            </div>
+            <div>
+              <h2>Post status</h2>
+              <div>
+                Your post is on verification. It may take an hour. Thank you for your patience!
+              </div>
+            </div>
+            <div>
+              <h2>Post status</h2>
+              <div>
+                Verification failed!
+                Reason: ...
+              </div>
+            </div>
+            <div>
+              <div>
+                <h2>Post status</h2>
+                <div>
+                  Post was verified on XX/XX/XX XX:XX
+                </div>
+              </div>
+              <div>
+                DO NOT delete and DO NOT modify your post for the next 24 hours. We'll be checking on it time to time.
+              </div>
+            </div>
+            <div>
+              <h2>Post status</h2>
+              <div>
+                Post was verified on XX/XX/XX XX:XX
+              </div>
+              <div>
+                Post check failed on XX/XX/XX XX:XX
+              </div>
+              <div>
+                Reason: the post was modified | we couldn't open the post
+              </div>
+            </div>
+            <div>
+              <h2>
+                Post status
+              </h2>
+              <div>
+                Post was verified on XX/XX/XX XX:XX
+              </div>
+              <div>
+                Post last check was on XX/XX/XX XX:XX
+              </div>
+              <div>
+                You've earned XXX TKN!
+              </div>
+              <div>
+                <button>CLAIM</button>
+              </div>
+            </div>
+          </div>
+
+
+          <h2>Full Details</h2>
+          <CampView item={camp} />
+        </>
+      }
     </MultiLoader>
   )
 }
