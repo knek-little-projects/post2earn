f838ffe1b0d60a4af452c887361ef3634de0877d
sknek <sknek@ya.ru>
2024-09-08 00:20:56 +0300

    camp and posts contracts

--- contracts/src/Campaign.sol
+++ contracts/src/Campaign.sol
@@ -1,5 +1,64 @@
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+struct BookingSchedule {
+    uint startTime;
+    uint startIndex;
+    uint maxMinutes;
+    uint[] counts;
+}
+
+library BookingLib {
+    function init(BookingSchedule storage a, uint maxMinutes) internal {
+        a.maxMinutes = maxMinutes;
+        a.counts = new uint[](maxMinutes);
+    }
+
+    function countActive(BookingSchedule storage a) internal view returns (uint) {
+        uint elapsedMinutes = (block.timestamp - a.startTime) / 1 minutes;
+
+        if (elapsedMinutes < a.maxMinutes) {
+            uint s = 0;
+            for (uint i = elapsedMinutes; i < a.maxMinutes; i++) {
+                s += a.counts[(i + a.startIndex) % a.maxMinutes];
+            }
+            return s;
+        }
+
+        return 0;
+    }
+
+    function schedule(BookingSchedule storage a) internal returns (uint expires) {
+        uint elapsedMinutes = (block.timestamp - a.startTime) / 1 minutes;
+
+        if (elapsedMinutes < a.maxMinutes) {
+            for (uint i = 0; i < elapsedMinutes; i++) {
+                a.counts[(i + a.startIndex) % a.maxMinutes] = 0;
+            }
+            a.startTime += elapsedMinutes * 1 minutes;
+            a.startIndex = (a.startIndex + elapsedMinutes) % a.maxMinutes;
+            a.counts[(a.startIndex + a.maxMinutes - 1) % a.maxMinutes]++;
+        } else {
+            a.startIndex = 0;
+            a.startTime = block.timestamp;
+            for (uint i = 0; i < a.maxMinutes; i++) {
+                a.counts[i] = 0;
+            }
+        }
+
+        expires = a.startTime + a.maxMinutes * 1 minutes;
+    }
+
+    function unschedule(BookingSchedule storage a, uint timestamp) internal {
+        // TODO: check the edge case
+        if (timestamp <= block.timestamp) {
+            return;
+        }
+
+        uint elapsed = timestamp - a.startTime;
+        a.counts[(a.startIndex + elapsed) % a.maxMinutes]--;
+    }
+}
+
 struct CampaignDescription {
     string title;
     string text;
@@ -11,17 +70,56 @@ struct CampaignDescription {
     uint paymentPerPost;
     uint maxPosts;
     uint paymentSupply;
+    // TODO: blogger cooloff period
+    // TODO: blogger net intersection
 }
 
 struct Campaign {
     address owner;
     uint started;
     uint finished;
-    uint postsInProgress;
-    uint postsSuccess;
+    uint forcefullyClosedSlots;
+    uint pipelinesFinished;
+    uint pipelinesInProgress;
+    BookingSchedule bookingSchedule;
     CampaignDescription description;
 }
 
+library CampaignLib {
+    function countUsedSlots(Campaign storage camp) internal view returns (uint) {
+        return
+            camp.forcefullyClosedSlots +
+            camp.pipelinesInProgress +
+            camp.pipelinesFinished +
+            BookingLib.countActive(camp.bookingSchedule);
+    }
+
+    function countFreeSlots(Campaign storage camp) internal view returns (uint) {
+        return
+            camp.description.maxPosts -
+            camp.pipelinesInProgress -
+            camp.forcefullyClosedSlots -
+            camp.pipelinesFinished -
+            BookingLib.countActive(camp.bookingSchedule);
+    }
+}
+
+enum PostStatus {
+    NOT_INITIALIZED,
+    BOOKED,
+    READY_FOR_ATTESTATION,
+    FIRST_CHECK_PASSED,
+    LAST_CHECK_PASSED,
+    FAILED
+}
+
 struct BlogPost {
-    // ...
+    PostStatus status;
+    uint bookingExpires;
+    address owner;
+    uint socialId;
+    uint postId;
+    string postUrl;
+    uint firstSeen;
+    uint lastSeen;
 }
--- contracts/src/Hook.sol
+++ contracts/src/Hook.sol
@@ -6,11 +6,14 @@ import "lib/sign-protocol-evm/src/interfaces/ISP.sol";
 import "lib/sign-protocol-evm/src/models/DataLocation.sol";
 
 import "lib/openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
+import "lib/openzeppelin-contracts-upgradeable/contracts/utils/PausableUpgradeable.sol";
 
 import "./Campaign.sol";
 
-contract Hook is ISPHook, OwnableUpgradeable {
+contract Hook is ISPHook, OwnableUpgradeable, PausableUpgradeable {
     using SafeERC20 for IERC20;
+    using BookingLib for BookingSchedule;
+    using CampaignLib for Campaign;
 
     event HookCalled(address sender, address attester, uint64 schemaId, uint64 attestationId);
 
@@ -20,12 +23,32 @@ contract Hook is ISPHook, OwnableUpgradeable {
     mapping(uint => Campaign) campaigns;
     uint public totalCampaigns;
 
+    mapping(address blogger => mapping(uint campaignId => BlogPost)) posts;
+    uint public totalPosts;
+
+    modifier onlyCampaignOwner(uint campaignId) {
+        require(campaigns[campaignId].started > 0, "I");
+        require(campaigns[campaignId].owner == msg.sender, "O");
+        _;
+    }
+
+    modifier onlyPostOwner(uint campaignId) {
+        require(posts[msg.sender][campaignId].status != PostStatus.NOT_INITIALIZED, "I");
+        require(posts[msg.sender][campaignId].owner == msg.sender, "O");
+        _;
+    }
+
     constructor() {
         _disableInitializers();
     }
 
     function initialize(address owner) public initializer {
         __Ownable_init(owner);
+        __Pausable_init();
+    }
+
+    function initializeV2() public reinitializer(2) {
+        __Pausable_init();
     }
 
     function getCampaignsPage(
@@ -57,23 +80,25 @@ contract Hook is ISPHook, OwnableUpgradeable {
         schemaId = id;
     }
 
-    function attest() external {
-        Attestation memory sample = Attestation({
-            schemaId: schemaId,
-            linkedAttestationId: 0,
-            attestTimestamp: 123,
-            revokeTimestamp: 0,
-            attester: address(this),
-            validUntil: 0,
-            dataLocation: DataLocation.ONCHAIN,
-            revoked: false,
-            recipients: new bytes[](0),
-            data: ""
-        });
-        sp.attest(sample, "", "", "");
-    }
+    // function attest() external {
+    //     Attestation memory sample = Attestation({
+    //         schemaId: schemaId,
+    //         linkedAttestationId: 0,
+    //         attestTimestamp: 123,
+    //         revokeTimestamp: 0,
+    //         attester: address(this),
+    //         validUntil: 0,
+    //         dataLocation: DataLocation.ONCHAIN,
+    //         revoked: false,
+    //         recipients: new bytes[](0),
+    //         data: ""
+    //     });
+    //     sp.attest(sample, "", "", "");
+    // }
 
-    function startCampaign(CampaignDescription calldata desc) external returns (uint campaignId) {
+    function startCampaign(CampaignDescription calldata desc) external whenNotPaused returns (uint campaignId) {
+        // TODO: review campaign
+
         if (address(desc.token) != address(0)) {
             require(desc.paymentSupply > 0, "P");
             desc.token.safeTransferFrom(msg.sender, address(this), desc.paymentSupply);
@@ -85,39 +110,88 @@ contract Hook is ISPHook, OwnableUpgradeable {
         camp.owner = msg.sender;
         camp.started = block.timestamp;
         camp.description = desc;
+        camp.bookingSchedule.init(5);
     }
 
-    function finishCampaign(uint id) external {
+    function finishCampaign(uint id) external onlyCampaignOwner(id) {
         Campaign storage camp = campaigns[id];
         require(camp.owner == msg.sender, "O");
         require(camp.finished == 0, "F");
         camp.finished = block.timestamp;
-        _flushCampaign(camp);
     }
 
-    function flushCampaign(uint id) external {
+    function flushCampaign(uint id) external onlyCampaignOwner(id) whenNotPaused {
         Campaign storage camp = campaigns[id];
-        require(camp.owner == msg.sender, "O");
-        _flushCampaign(camp);
+        require(camp.finished != 0, "F");
+
+        uint freeSlots = camp.countFreeSlots();
+        camp.forcefullyClosedSlots += freeSlots;
+
+        uint withdrawableFunds = freeSlots * camp.description.paymentPerPost;
+        camp.description.token.safeTransfer(camp.owner, withdrawableFunds);
     }
 
-    function _flushCampaign(Campaign storage camp) internal {
-        uint unavailable = (camp.postsInProgress + camp.postsSuccess) *
-            camp.description.paymentPerPost;
-        if (camp.description.paymentSupply <= unavailable) {
-            return;
-        }
-        uint availableFunds = camp.description.paymentSupply - unavailable;
-        camp.description.token.safeTransfer(camp.owner, availableFunds);
-        camp.description.paymentSupply -= availableFunds;
+    function book(uint campaignId) external whenNotPaused {
+        Campaign storage camp = campaigns[campaignId];
+        require(camp.started > 0, "S");
+        require(camp.finished == 0, "F");
+        require(camp.countFreeSlots() > 0, "L");
+        require(posts[msg.sender][campaignId].status == PostStatus.NOT_INITIALIZED, "B");
+
+        BlogPost storage post = posts[msg.sender][campaignId];
+        post.status = PostStatus.BOOKED;
+        post.bookingExpires = camp.bookingSchedule.schedule();
+        post.owner = msg.sender;
+        post.socialId = 0;
+        post.postId = totalPosts++;
     }
 
-    // book(id)
-    // post(link) postId
-    // verify(postId)
-    // fail(postId, reason)
-    // success(postId)
-    // claim(postId)
+    function post(uint campaignId, string calldata postUrl) external whenNotPaused onlyPostOwner(campaignId) {
+        BlogPost storage post = posts[msg.sender][campaignId];
+        require(post.status == PostStatus.BOOKED, "B");
+        require(block.timestamp < post.bookingExpires, "E");
+
+        Campaign storage camp = campaigns[campaignId];
+        camp.bookingSchedule.unschedule(post.bookingExpires);
+        delete post.bookingExpires;
+        camp.pipelinesInProgress++;
+        
+        post.status = PostStatus.READY_FOR_ATTESTATION;
+        post.postUrl = postUrl;
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    function postNotOK(address blogger, uint campaignId, string calldata reason) external onlyOwner {
+
+    }
+
+    function postOK(address blogger, uint campaignId, uint timestamp) external onlyOwner {
+        // BlogPost storage post = posts[blogger][campaignId];
+        //         Attestation memory sample = Attestation({
+        //     schemaId: schemaId,
+        //     linkedAttestationId: 0,
+        //     attestTimestamp: 123,
+        //     revokeTimestamp: 0,
+        //     attester: address(this),
+        //     validUntil: 0,
+        //     dataLocation: DataLocation.ONCHAIN,
+        //     revoked: false,
+        //     recipients: new bytes[](0),
+        //     data: ""
+        // });
+        // sp.attest(sample, "", "", "");
+    }
+
+    function claim(uint campaignId) external onlyPostOwner(campaignId) {
+        //
+    }
 
     function didReceiveAttestation(
         address attester,
